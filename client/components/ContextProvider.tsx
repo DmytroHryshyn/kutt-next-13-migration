22d2f4e05b66c586a698fd474c6fd63692cb2088
dhryshyn <dev.dmytroh@gmail.com>
2023-05-16 20:00:00 +0300

    Bootstrapping: bump styled-component, add styled-components registry

--- client/components/ContextProvider.tsx
+++ client/components/ContextProvider.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { StoreProvider } from "easy-peasy";
 import { initializeStore }  from '../store';
+import StyledComponentsRegistry from "./StyledComponentsRegistry";
 
 type Props = {
   children: React.ReactNode;
@@ -10,7 +11,11 @@ const store = initializeStore();
 
 const ContextProvider = ({ children }: Props) => {
   return (
-    <StoreProvider store={store}>{ children }</StoreProvider>
+    <StoreProvider store={store}>
+      <StyledComponentsRegistry>
+        { children }
+      </StyledComponentsRegistry>
+     </StoreProvider>
   )
 }
 
