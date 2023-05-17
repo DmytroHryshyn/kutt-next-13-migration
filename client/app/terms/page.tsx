2c5d8c9fc1016aca800bf06376e3679c47d48b86
dhryshyn <dev.dmytroh@gmail.com>
2023-05-17 13:09:54 +0300

    Move pages to app: terms

--- client/app/terms/page.tsx
+++ client/app/terms/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import AppWrapper from "../components/AppWrapper";
-import { Col } from "../components/Layout";
+import AppWrapper from "../../components/AppWrapper";
+import { Col } from "../../components/Layout";
 
 const TermsPage = () => (
   <AppWrapper>
